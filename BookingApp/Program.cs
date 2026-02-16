cfb3733afe6fc0eafcf54d0302b408c61bf35112
RegiLoshi <rloshi111@gmail.com>
2026-02-16 21:30:44 +0100

    Removed swagger configs

--- BookingApp/Program.cs
+++ BookingApp/Program.cs
@@ -5,37 +5,22 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.RegisterInfrastructure(builder.Configuration);
 builder.Services.RegisterApplication();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 
-app.MapGet("/", () => Results.Ok(new { message = "Welcome to Booking API" }))
-    .WithName("GetRoot")
-    .WithTags("Health");
+app.MapGet("/", () => Results.Ok(new { message = "Welcome to Booking API" }));
 
-app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }))
-    .WithName("GetHealth")
-    .WithTags("Health");
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }));
 
-app.MapPost("/users/register", async (RegisterUserCommand command, IMediator mediator) =>
+app.MapPost("v1/user/register", async (RegisterUserCommand command, IMediator mediator) =>
 {
     var result = await mediator.Send(command);
     return Results.Ok(new { id = result });
-})
-.WithName("RegisterUser")
-.WithTags("Users");
+});
 
 app.Run();
